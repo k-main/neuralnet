10297f0d0ca042743f517ca3d407fa8822706c56
k-main <kennkmaina@gmail.com>
2025-04-21 19:45:49 -0700

    func to free memory used by model

--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 
 int main(int argc, char* argv[]) {
 
-    struct model Model = createModel(1, 1, 1, 3);
+    const unsigned int input_n = 3, output_n = 3, hidden_n = 3, sz_hidden = 5;
+    struct model Model = createModel(input_n, output_n, hidden_n, sz_hidden);
 
-
+    tearDown(&Model);
 
     return 0;
 }
--- model.h
+++ model.h
@@ -12,6 +12,7 @@ struct layer {
 };
 
 struct model {
+    unsigned int input_n, output_n, hidden_n, hidden_n_sz;
     struct layer input_layer;
     struct layer output_layer;
     struct layer* hidden_layers;
@@ -40,6 +41,12 @@ struct layer Layer(unsigned int n_neurons, int sizeof_prev_layer){
 struct model createModel(unsigned int n_inputs, unsigned int n_outputs, unsigned int n_hidden, unsigned int sz_hidden) {
     const unsigned int n_layers = n_inputs + n_outputs + n_hidden;
     struct model m;
+
+    m.input_n = n_inputs;
+    m.output_n = n_outputs;
+    m.hidden_n = n_hidden;
+    m.hidden_n_sz = sz_hidden;
+
     m.input_layer = Layer(n_inputs, 0);
     m.output_layer = Layer(n_outputs, sz_hidden);
     struct layer* hidden_layers = malloc(n_hidden * sizeof(struct layer));
@@ -56,4 +63,29 @@ struct model createModel(unsigned int n_inputs, unsigned int n_outputs, unsigned
 
     m.hidden_layers = hidden_layers;
     return m;
+}
+
+void tearDown(struct model* Model){
+    for (unsigned int i = 0; i < Model->input_n; i++) {
+        printf("Input Neuron %d => \t bias: %f, output: %f\n", i, Model->input_layer.neurons[i].bias, Model->input_layer.neurons[i].output);
+        free(Model->input_layer.neurons[i].weights);
+    }
+    free(Model->input_layer.neurons);
+
+    for (unsigned int i = 0; i < Model->hidden_n; i++) {
+        printf("Hidden Layer %d:\n", i);
+        for (unsigned int j = 0; j < Model->hidden_n_sz; j++) {
+            const struct neuron n = Model->hidden_layers[i].neurons[j];
+            printf("\t Neuron %d => \t bias: %f, output: %f\n", j, n.bias, n.output);
+            free(Model->hidden_layers[i].neurons[j].weights);
+        }
+        free(Model->hidden_layers[i].neurons);
+    }
+    free(Model->hidden_layers);
+
+    for (unsigned int i = 0; i < Model->output_n; i++) {
+        printf("Output Neuron %d => \t bias: %f, output: %f\n", i, Model->output_layer.neurons[i].bias, Model->input_layer.neurons[i].output);
+        free(Model->output_layer.neurons[i].weights);
+    }
+    free(Model->output_layer.neurons);
 }
